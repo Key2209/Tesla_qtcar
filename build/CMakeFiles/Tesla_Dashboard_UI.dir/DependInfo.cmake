df6540330d94514c3d5072d20ec03c002537dc8f
alientek <example@email.com>
2026-04-28 14:28:17 +0800

    Windows移植Linux，成功

---
+++ build/CMakeFiles/Tesla_Dashboard_UI.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alientek/桌面/new_pj/Controllers/MusicController.cpp" "/home/alientek/桌面/new_pj/build/CMakeFiles/Tesla_Dashboard_UI.dir/Controllers/MusicController.cpp.o"
+  "/home/alientek/桌面/new_pj/Controllers/PhoneController.cpp" "/home/alientek/桌面/new_pj/build/CMakeFiles/Tesla_Dashboard_UI.dir/Controllers/PhoneController.cpp.o"
+  "/home/alientek/桌面/new_pj/build/Tesla_Dashboard_UI_autogen/EWIEGA46WW/qrc_qml.cpp" "/home/alientek/桌面/new_pj/build/CMakeFiles/Tesla_Dashboard_UI.dir/Tesla_Dashboard_UI_autogen/EWIEGA46WW/qrc_qml.cpp.o"
+  "/home/alientek/桌面/new_pj/build/Tesla_Dashboard_UI_autogen/mocs_compilation.cpp" "/home/alientek/桌面/new_pj/build/CMakeFiles/Tesla_Dashboard_UI.dir/Tesla_Dashboard_UI_autogen/mocs_compilation.cpp.o"
+  "/home/alientek/桌面/new_pj/main.cpp" "/home/alientek/桌面/new_pj/build/CMakeFiles/Tesla_Dashboard_UI.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CONCURRENT_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_QMLMODELS_LIB"
+  "QT_QML_LIB"
+  "QT_QUICK_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "Tesla_Dashboard_UI_autogen/include"
+  "../CPP"
+  "/opt/Qt/5.15.2/gcc_64/include"
+  "/opt/Qt/5.15.2/gcc_64/include/QtCore"
+  "/opt/Qt/5.15.2/gcc_64/./mkspecs/linux-g++"
+  "/opt/Qt/5.15.2/gcc_64/include/QtQuick"
+  "/opt/Qt/5.15.2/gcc_64/include/QtQmlModels"
+  "/opt/Qt/5.15.2/gcc_64/include/QtQml"
+  "/opt/Qt/5.15.2/gcc_64/include/QtNetwork"
+  "/opt/Qt/5.15.2/gcc_64/include/QtGui"
+  "/opt/Qt/5.15.2/gcc_64/include/QtConcurrent"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Tesla_Dashboard_UI.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tesla_Dashboard_UI_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Tesla_Dashboard_UI_autogen.dir/ParseCache.txt"
+  "Tesla_Dashboard_UI_autogen"
+  "CMakeFiles/Tesla_Dashboard_UI.dir/Controllers/MusicController.cpp.o"
+  "CMakeFiles/Tesla_Dashboard_UI.dir/Controllers/PhoneController.cpp.o"
+  "CMakeFiles/Tesla_Dashboard_UI.dir/Tesla_Dashboard_UI_autogen/EWIEGA46WW/qrc_qml.cpp.o"
+  "CMakeFiles/Tesla_Dashboard_UI.dir/Tesla_Dashboard_UI_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Tesla_Dashboard_UI.dir/main.cpp.o"
+  "Tesla_Dashboard_UI"
+  "Tesla_Dashboard_UI.pdb"
+  "Tesla_Dashboard_UI_autogen/EWIEGA46WW/qrc_qml.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tesla_Dashboard_UI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
